81ecd4d3c07fc2fe180ebc98d3f7cbf74a1b78fe
root <root@UbuntuUptest.cygdc.local>
2021-06-30 00:37:47 -0400

    change in script file in build

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,5 +4,5 @@ echo "***************************"
 echo "********Testing************"
 echo "***************************"
 WORKSPACE=/home/cyg_adm/jenkins_home/workspace/maven-pipeline
-docker run --rm -ti -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.6.0-jdk-8 "$@"
+docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.6.0-jdk-8 "$@"
 
