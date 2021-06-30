60813c94c4dd6d7594b5b1971f21b685a508304a
root <root@UbuntuUptest.cygdc.local>
2021-06-30 00:28:01 -0400

    change in workspace

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -3,5 +3,5 @@
 echo "*******************************"
 echo "******Building**********"
 echo "********************************"
-
-docker run --rm -ti -v $PWD/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.6.0-jdk-8 "$@"
+WORKSPACE=/home/cyg_adm/jenkins_home/workspace/maven-pipeline 
+docker run --rm -ti -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.6.0-jdk-8 "$@"
