b8c03ee265c5efaf67ffa880e088ff1bd1590f86
root <root@UbuntuUptest.cygdc.local>
2021-09-06 00:32:13 -0400

    change in workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,4 +5,5 @@ echo "*****JAR Creation*************"
 echo "******************************"
 
 
-docker run --rm -v /home/cyg_adm/pipeline-maven-project/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-jdk-11 "$@"
+WORKSPACE=/home/cyg_adm/jenkins_home/workspace/pipeline-maven-project
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-jdk-11 "$@"
