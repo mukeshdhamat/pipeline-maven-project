6ca27b4aa1205b840a27e50bdf9c60fe6dc423ff
root <root@UbuntuUptest.cygdc.local>
2021-09-06 22:49:37 -0400

    change in deploy file

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,3 +7,4 @@ echo $PASS >> /tmp/.auth
 
 scp -i /opt/id_rsa /tmp/.auth prod-user@172.19.10.232:/tmp/.auth
 scp -i /opt/id_rsa jenkins/deploy/publish.sh prod-user@172.19.10.232:/tmp/publish.sh
+ssh -i /opt/prod01 prod-user@172.19.10.232 "/tmp/publish.sh"
